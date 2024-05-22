17d59ea052123065326b7451246b5bd34bff873a
Patryk Stępień <fighting.dreamer.0@gmail.com>
2024-05-23 00:04:35 +0200

    lint(gitsigns): reformat

--- lua/nv/lua/plugin/git/gitsigns.lua
+++ lua/nv/lua/plugin/git/gitsigns.lua
@@ -1,39 +1,39 @@
 local map = vim.keymap.set
 
 local opts = function()
-    return {
-        -- │┆┃┇_‾~
-        signs = {
-            add          = { text = "┃" },
-            change       = { text = "┃" },
-            delete       = { text = '_' },
-            topdelete    = { text = "‾" },
-            changedelete = { text = "~" },
-            untracked    = { text = '┇' },
-        },
+  return {
+    -- │┆┃┇_‾~
+    signs = {
+      add = { text = "┃" },
+      change = { text = "┃" },
+      delete = { text = "_" },
+      topdelete = { text = "‾" },
+      changedelete = { text = "~" },
+      untracked = { text = "┇" },
+    },
 
-        on_attach = function(bufnr)
-            local gs = package.loaded.gitsigns
+    on_attach = function(bufnr)
+      local gs = package.loaded.gitsigns
 
-            local function opts(desc)
-                return { buffer = bufnr, desc = desc }
-            end
+      local function opts(desc)
+        return { buffer = bufnr, desc = desc }
+      end
 
-            map("n", "<leader>rh", gs.reset_hunk, opts "Reset Hunk")
-            map("n", "<leader>ph", gs.preview_hunk, opts "Preview Hunk")
-            map("n", "<leader>gb", gs.blame_line, opts "Blame Line")
-        end,
-    }
+      map("n", "<leader>rh", gs.reset_hunk, opts "Reset Hunk")
+      map("n", "<leader>ph", gs.preview_hunk, opts "Preview Hunk")
+      map("n", "<leader>gb", gs.blame_line, opts "Blame Line")
+    end,
+  }
 end
 
 local config = function(_, opts)
-    require('gitsigns').setup(opts)
+  require("gitsigns").setup(opts)
 end
 
 return {
-    -- Super fast git decorations implemented purely in Lua.
-    "lewis6991/gitsigns.nvim",
-    config = config,
-    event = 'BufReadPost',
-    opts = opts
+  -- Super fast git decorations implemented purely in Lua.
+  "lewis6991/gitsigns.nvim",
+  config = config,
+  event = "BufReadPost",
+  opts = opts,
 }
