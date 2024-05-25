13076af3d0b24442a079384f956988b3c4f521d7
Patryk Stępień <fighting.dreamer.0@gmail.com>
2024-05-25 02:27:53 +0200

    feat(diagnostic): implement toggle

--- lua/nv/lua/core/opts_diagnostic.lua
+++ lua/nv/lua/core/opts_diagnostic.lua
@@ -1,8 +1,13 @@
-local diagnostic = vim.diagnostic
+local function toggle_inline_diagnostic()
+  local enabled = vim.diagnostic.config().virtual_text
+  vim.diagnostic.config { virtual_text = not enabled }
+end
 
-diagnostic.config({
-    virtual_text = false,
-    underline = {
-        severity = diagnostic.severity.ERROR,
-    }
-})
+vim.keymap.set("n", "<leader>td", toggle_inline_diagnostic, { desc = "lsp virtual text toggle" })
+
+vim.diagnostic.config {
+  virtual_text = true,
+  underline = {
+    severity = vim.diagnostic.severity.ERROR,
+  },
+}
