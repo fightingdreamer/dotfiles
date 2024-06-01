ac68183c20e1fd3c7d49469a8d1b71e0cdd2c30a
Patryk Stępień <fighting.dreamer.0@gmail.com>
2024-06-01 19:18:01 +0200

    feat(trouble): add mappings

--- lua/nv/lua/plugin/ui/trouble.lua
+++ lua/nv/lua/plugin/ui/trouble.lua
@@ -10,6 +10,12 @@ return {
   "folke/trouble.nvim",
   config = config,
   branch = "dev", -- 3.0 beta
+  keys = {
+    { "<leader><leader>t", "<cmd>Trouble diagnostics toggle filter.buf=0<cr>", desc = "trouble: buffer diagnostics" },
+    { "<leader><leader>T", "<cmd>Trouble diagnostics toggle<cr>", desc = "trouble: diagnostics" },
+    { "<leader><leader>s", "<cmd>Trouble symbols toggle focus=false<cr>", desc = "trouble: Symbols" },
+    { "<leader><leader>l", "<cmd>Trouble lsp toggle focus=false win.position=right<cr>", desc = "trouble: lsp diagnostics" },
+  },
   opts = opts,
   cmd = "Trouble",
 }
