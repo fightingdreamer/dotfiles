314922349757d98b26e043fdf65cf4b1fc4121f4
Patryk Stępień <fighting.dreamer.0@gmail.com>
2024-06-19 22:51:28 +0200

    feat(spectre): init

---
+++ lua/nv/lua/plugin/editing/nvim-spectre.lua
@@ -0,0 +1,34 @@
+local function opts()
+  return {}
+end
+
+local function config(_, opts)
+  require("spectre").setup(opts)
+end
+
+local function replace_current_word()
+  require("spectre").open_visual { select_word = true }
+end
+
+local function replace_selection()
+  require("spectre").open_visual()
+end
+
+local function replace_in_file()
+  require("spectre").open_file_search { select_word = true }
+end
+
+return {
+  -- Find the enemy and replace them with dark power
+  "nvim-pack/nvim-spectre",
+  dependencies = { "nvim-lua/plenary.nvim" },
+  version = false,
+  config = config,
+  opts = opts,
+  keys = {
+    { "<leader>S", replace_current_word, mode = "n", desc = "Spectre: search current word" },
+    { "<leader>S", replace_selection, mode = "v", desc = "Spectre: search current word" },
+    { "\\S", replace_in_file, mode = "n", desc = "Spectre: search in current file" },
+  },
+  cmd = "Spectre",
+}
