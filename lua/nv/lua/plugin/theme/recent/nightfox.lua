0ecebe052daa92994c914d71f533d66f6047e887
Patryk Stępień <fighting.dreamer.0@gmail.com>
2024-08-29 16:07:47 +0200

    feat(theme): switch to duskfox

--- lua/nv/lua/plugin/theme/recent/nightfox.lua
+++ lua/nv/lua/plugin/theme/recent/nightfox.lua
@@ -43,16 +43,16 @@ local config = function(_, opts)
   require("nightfox").setup(opts)
 
   -- light
-  vim.opt.background = "light"
-  vim.cmd.colorscheme "dayfox"
+  -- vim.opt.background = "light"
+  -- vim.cmd.colorscheme "dayfox"
 
   -- dark
   -- vim.opt.background = "dark"
   -- vim.cmd.colorscheme "nightfox"
 
   -- dark
-  -- vim.opt.background = "dark"
-  -- vim.cmd.colorscheme "duskfox"
+  vim.opt.background = "dark"
+  vim.cmd.colorscheme "duskfox"
 end
 
 return {
