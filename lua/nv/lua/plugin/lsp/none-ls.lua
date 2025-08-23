8d013074025020f6fa31eb3705335278ab0ecce8
Patryk Stępień <fighting.dreamer.0@gmail.com>
2025-08-23 20:31:38 +0200

    feat(none-ls): add plugin

---
+++ lua/nv/lua/plugin/lsp/none-ls.lua
@@ -0,0 +1,26 @@
+local _config = function()
+  local null_ls = require "null-ls"
+  null_ls.setup {
+    sources = {
+      null_ls.builtins.formatting.stylua,
+      null_ls.builtins.completion.spell,
+      -- >> ~/.config/mypy/config
+      -- [mypy]
+      -- python_executable = .venv/bin/python
+      -- >> ./pyproject.toml
+      -- [tool.mypy]
+      -- python_executable = ".venv/bin/python"
+      null_ls.builtins.diagnostics.mypy,
+    },
+  }
+end
+
+return {
+  "nvimtools/none-ls.nvim",
+  dependencies = {
+    "nvimtools/none-ls-extras.nvim",
+  },
+  event = "BufReadPost",
+  config = _config,
+  keys = {},
+}
