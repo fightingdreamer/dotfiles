dd8946837f0a5e0c115e271500a922f587eea88c
Patryk Stępień <fighting.dreamer.0@gmail.com>
2025-08-23 23:50:36 +0200

    chore(none-ls): reformat

--- lua/nv/lua/plugin/lsp/none-ls.lua
+++ lua/nv/lua/plugin/lsp/none-ls.lua
@@ -4,12 +4,15 @@ local _config = function()
     sources = {
       null_ls.builtins.formatting.stylua,
       null_ls.builtins.completion.spell,
+      --
       -- >> ~/.config/mypy/config
       -- [mypy]
       -- python_executable = .venv/bin/python
+      --
       -- >> ./pyproject.toml
       -- [tool.mypy]
       -- python_executable = ".venv/bin/python"
+      --
       null_ls.builtins.diagnostics.mypy,
     },
   }
