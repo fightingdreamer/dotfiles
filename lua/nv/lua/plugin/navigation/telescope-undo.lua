6357283061ab7bc5842300394dda64dfe7418d6c
Patryk Stępień <fighting.dreamer.0@gmail.com>
2025-08-24 03:44:30 +0200

    feat(telescope): disable undo plugin

--- lua/nv/lua/plugin/navigation/telescope-undo.lua
+++ lua/nv/lua/plugin/navigation/telescope-undo.lua
@@ -1,32 +1,33 @@
 local opts = function()
-    return {
-        extensions = {
-            undo = {
-                use_delta = false,
-                mappings = {
-                    i = {
-                        ["<cr>"] = require("telescope-undo.actions").restore,
-                    },
-                    n = {
-                        ["<cr>"] = require("telescope-undo.actions").restore,
-                    }
-                }
-            }
+  return {
+    extensions = {
+      undo = {
+        use_delta = false,
+        mappings = {
+          i = {
+            ["<cr>"] = require("telescope-undo.actions").restore,
+          },
+          n = {
+            ["<cr>"] = require("telescope-undo.actions").restore,
+          },
         },
-    }
+      },
+    },
+  }
 end
 
 local config = function(_, opts)
-    require("telescope").setup(opts)
-    require("telescope").load_extension("undo")
+  require("telescope").setup(opts)
+  require("telescope").load_extension "undo"
 end
 
 return {
-    "debugloop/telescope-undo.nvim",
-    dependencies = { "nvim-telescope/telescope.nvim" },
-    config = config,
-    opts = opts,
-    keys = {
-        { "<leader>u", "<cmd>Telescope undo<cr>", desc = "Telescope undo" },
-    }
+  "debugloop/telescope-undo.nvim",
+  dependencies = { "nvim-telescope/telescope.nvim" },
+  config = config,
+  opts = opts,
+  keys = {
+    { "<leader>u", "<cmd>Telescope undo<cr>", desc = "Telescope undo" },
+  },
+  enabled = false,
 }
