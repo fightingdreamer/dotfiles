cea28f7b8949dd1ad4e052e0f828f104538d0d6a
Patryk Stępień <fighting.dreamer.0@gmail.com>
2025-08-27 18:53:05 +0200

    feat(core): change lsp-rename mapping

--- lua/nv/lua/core/opts_lsp.lua
+++ lua/nv/lua/core/opts_lsp.lua
@@ -139,7 +139,7 @@ local function lsp_attach(args)
   -- end
 
   if lsp_have_feature.rename(client) then
-    vim.keymap.set("n", "<leader>R", lsp_buf_rename_use_priority_or_select, { buffer = bufnr, desc = "lsp rename" })
+    vim.keymap.set("n", "<leader>lR", lsp_buf_rename_use_priority_or_select, { buffer = bufnr, desc = "lsp rename" })
   end
 
   if lsp_have_feature.inlay_hint(client) then
