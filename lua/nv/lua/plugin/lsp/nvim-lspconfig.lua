cad029bf6bb31fcb0f6e335b300084590aee43b8
Patryk Stępień <fighting.dreamer.0@gmail.com>
2025-09-01 21:14:11 +0200

    feat(lsp): revert to more stable basedpyright

--- lua/nv/lua/plugin/lsp/nvim-lspconfig.lua
+++ lua/nv/lua/plugin/lsp/nvim-lspconfig.lua
@@ -157,8 +157,8 @@ local function get_servers()
     -- Python.
     -- ty = {},
     ruff = get_ruff(),
-    pyrefly = {},
-    -- basedpyright = get_basedpyright(),
+    -- pyrefly = {},
+    basedpyright = get_basedpyright(),
 
     -- Yaml.
     yamlls = {},
