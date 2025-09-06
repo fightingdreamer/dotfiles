9a5540e3edd6f6c60af3c6012635c2911692e9f1
Patryk Stępień <fighting.dreamer.0@gmail.com>
2025-09-06 21:08:28 +0200

    Add mapping for some git operations

--- lua/nv/lua/plugin/gitsigns.lua
+++ lua/nv/lua/plugin/gitsigns.lua
@@ -1,5 +1,44 @@
 -- Super fast git decorations implemented purely in Lua.
 
+local m = {
+  prev_hunk = function()
+    require("gitsigns").nav_hunk "prev"
+  end,
+  next_hunk = function()
+    require("gitsigns").nav_hunk "next"
+  end,
+  reset_hunk = function()
+    local gitsigns = require "gitsigns"
+    if vim.fn.mode() == "n" then
+      gitsigns.reset_hunk()
+    end
+    if vim.fn.mode() == "V" then
+      gitsigns.reset_hunk {
+        vim.fn.line ".",
+        vim.fn.line "v",
+      }
+    end
+  end,
+  stage_hunk = function()
+    local gitsigns = require "gitsigns"
+    if vim.fn.mode() == "n" then
+      gitsigns.stage_hunk()
+    end
+    if vim.fn.mode() == "V" then
+      gitsigns.stage_hunk {
+        vim.fn.line ".",
+        vim.fn.line "v",
+      }
+    end
+  end,
+  blame = function()
+    require("gitsigns").blame()
+  end,
+  toggle_current_line_blame = function()
+    require("gitsigns").toggle_current_line_blame()
+  end,
+}
+
 return {
   "lewis6991/gitsigns.nvim",
   opts = {
@@ -10,7 +49,20 @@ return {
       delete = { text = "_" },
       topdelete = { text = "‾" },
       changedelete = { text = "~" },
-      untracked = { text = "┇" },
+      untracked = { text = "┆" },
+    },
+    signs_staged = {
+      add = { text = "┃" },
+      change = { text = "┃" },
+      delete = { text = "_" },
+      topdelete = { text = "‾" },
+      changedelete = { text = "~" },
+      untracked = { text = "┆" },
+    },
+    diff_opts = {
+      internal = true,
+      algorithm = "histogram",
+      linematch = 1,
     },
     watch_gitdir = {
       enable = true,
@@ -35,6 +87,11 @@ return {
 
   event = "VeryLazy",
   keys = {
-    { "\\g", "<cmd>Gitsigns toggle_current_line_blame<cr>", mode = { "n" }, desc = "Gitsigns toggle current line blame" },
+    { "[h", m.prev_hunk, mode = { "n" }, desc = "Navigate to previous hunk" },
+    { "]h", m.next_hunk, mode = { "n" }, desc = "Navigate to next hunk" },
+    { "gr", m.reset_hunk, mode = { "n", "v" }, desc = "Reset hunk" },
+    { "gs", m.stage_hunk, mode = { "n", "v" }, desc = "Stage hunk" },
+    { "gB", m.blame, mode = { "n" }, desc = "Toggle git blame" },
+    { "\\b", m.toggle_current_line_blame, mode = { "n" }, desc = "Gitsigns toggle current line blame" },
   },
 }
