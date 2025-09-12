0a5f531446d5cc70b7dafa87ff08cf872d471af5
Patryk Stępień <fighting.dreamer.0@gmail.com>
2025-09-12 18:06:25 +0200

    Limit cursorline and cursorcolumn to current window

--- lua/nv/lua/core/cmds_user.lua
+++ lua/nv/lua/core/cmds_user.lua
@@ -1,4 +1,12 @@
-local api = vim.api
+vim.api.nvim_create_user_command("W", ":w", {})
+vim.api.nvim_create_user_command("Q", ":qall!", {})
 
-api.nvim_create_user_command('W', ':w', {})
-api.nvim_create_user_command('Q', ':qall!', {})
+vim.api.nvim_create_autocmd("WinEnter", {
+  pattern = { "*" },
+  command = "set cursorline cursorcolumn",
+})
+
+vim.api.nvim_create_autocmd("WinLeave", {
+  pattern = { "*" },
+  command = "set nocursorline nocursorcolumn",
+})
