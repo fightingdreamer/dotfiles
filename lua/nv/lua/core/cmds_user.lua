522dab86cabf7113020a0eee83689248e7018c4d
Patryk Stępień <fighting.dreamer.0@gmail.com>
2025-10-05 21:25:57 +0200

    Improve cursor column

--- lua/nv/lua/core/cmds_user.lua
+++ lua/nv/lua/core/cmds_user.lua
@@ -1,12 +1,18 @@
 vim.api.nvim_create_user_command("W", ":w", {})
 vim.api.nvim_create_user_command("Q", ":qall!", {})
 
-vim.api.nvim_create_autocmd("WinEnter", {
+vim.api.nvim_create_autocmd({ "WinEnter", "BufWinEnter", "BufEnter", "TabEnter" }, {
   pattern = { "*" },
-  command = "set cursorline cursorcolumn",
+  callback = function()
+    vim.opt_local.cursorline = true
+    vim.opt_local.cursorcolumn = true
+  end,
 })
 
-vim.api.nvim_create_autocmd("WinLeave", {
+vim.api.nvim_create_autocmd({ "WinLeave", "BufLeave", "TabLeave" }, {
   pattern = { "*" },
-  command = "set nocursorline nocursorcolumn",
+  callback = function()
+    vim.opt_local.cursorline = false
+    vim.opt_local.cursorcolumn = false
+  end,
 })
