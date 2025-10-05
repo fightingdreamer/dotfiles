28094e1e9d9a3aeb97fb7efce0f33e48f89acd86
Patryk Stępień <fighting.dreamer.0@gmail.com>
2025-10-05 21:35:48 +0200

    Enable mypy in none-ls as example

--- lua/nv/lua/plugin/disabled/none-ls.lua
+++ lua/nv/lua/plugin/disabled/none-ls.lua
@@ -1,27 +1,27 @@
-local _config = function()
-  local null_ls = require "null-ls"
-  null_ls.setup {
-    sources = {
-      null_ls.builtins.formatting.stylua,
-      null_ls.builtins.completion.spell,
-      --
-      -- >> ~/.config/mypy/config
-      -- [mypy]
-      -- python_executable = .venv/bin/python
-      --
-      -- >> ./pyproject.toml
-      -- [tool.mypy]
-      -- python_executable = ".venv/bin/python"
-      --
-      null_ls.builtins.diagnostics.mypy,
-    },
-  }
-end
+-- Use Neovim as a language server to inject LSP diagnostics, code actions, and more via Lua.
 
 return {
   "nvimtools/none-ls.nvim",
-  event = "BufReadPost",
-  config = _config,
-  keys = {},
-  enabled = false,
+
+  config = function()
+    local null_ls = require "null-ls"
+    null_ls.setup {
+      sources = {
+        -- null_ls.builtins.formatting.stylua,
+        -- null_ls.builtins.completion.spell,
+        --
+        -- >> ~/.config/mypy/config
+        -- [mypy]
+        -- python_executable = .venv/bin/python
+        --
+        -- >> ./pyproject.toml
+        -- [tool.mypy]
+        -- python_executable = ".venv/bin/python"
+        --
+        null_ls.builtins.diagnostics.mypy,
+      },
+    }
+  end,
+
+  event = "VeryLazy",
 }
