9281765732c848a04fbf65f7e8de95d0755080ef
Patryk Stępień <fighting.dreamer.0@gmail.com>
2025-10-05 21:27:24 +0200

    Improve debugging

--- lua/nv/lua/plugin/nvim-dap-view.lua
+++ lua/nv/lua/plugin/nvim-dap-view.lua
@@ -1,11 +1,45 @@
 -- Visualize debugging sessions in neovim.
 
+local m = {
+  open = function()
+    require("dap-view").open()
+  end,
+
+  close = function()
+    require("dap-view").close()
+  end,
+
+  toggle = function()
+    require("dap-view").toggle()
+  end,
+
+  add_expr = function(expr)
+    require("dap-view").add_expr(expr)
+  end,
+
+  jump_to_view = function(view)
+    require("dap-view").jump_to_view(view)
+  end,
+
+  show_view = function(view)
+    require("dap-view").show_view(view)
+  end,
+
+  navigate = function(opts)
+    require("dap-view").navigate(opts)
+  end,
+}
+
 return {
   "igorlfs/nvim-dap-view",
 
   ---@module 'dap-view'
   ---@type dapview.Config
   opts = {},
+
+  keys = {
+    { "\\d", m.toggle, mode = { "n" }, desc = "Toggle dap view" },
+  },
 
   cmd = {
     "DapViewOpen",
