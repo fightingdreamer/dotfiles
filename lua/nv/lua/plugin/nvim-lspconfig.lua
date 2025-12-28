1921873fa05f206ac223b8660870e34ab33c2883
Patryk Stępień <fighting.dreamer.0@gmail.com>
2025-12-28 18:35:58 +0100

    Enable inlay hints for typescript and javascript

--- lua/nv/lua/plugin/nvim-lspconfig.lua
+++ lua/nv/lua/plugin/nvim-lspconfig.lua
@@ -47,6 +47,34 @@ local c = {
       "typescriptreact",
       "vue",
     },
+    settings = {
+      typescript = {
+        inlayHints = {
+          -- You can set this to 'all' or 'literals' to enable more hints
+          includeInlayParameterNameHints = "all", -- 'none' | 'literals' | 'all'
+          includeInlayParameterNameHintsWhenArgumentMatchesName = false,
+          includeInlayFunctionParameterTypeHints = false,
+          includeInlayVariableTypeHints = false,
+          includeInlayVariableTypeHintsWhenTypeMatchesName = false,
+          includeInlayPropertyDeclarationTypeHints = false,
+          includeInlayFunctionLikeReturnTypeHints = true,
+          includeInlayEnumMemberValueHints = true,
+        },
+      },
+      javascript = {
+        inlayHints = {
+          -- You can set this to 'all' or 'literals' to enable more hints
+          includeInlayParameterNameHints = "all", -- 'none' | 'literals' | 'all'
+          includeInlayParameterNameHintsWhenArgumentMatchesName = false,
+          includeInlayVariableTypeHints = false,
+          includeInlayFunctionParameterTypeHints = false,
+          includeInlayVariableTypeHintsWhenTypeMatchesName = false,
+          includeInlayPropertyDeclarationTypeHints = false,
+          includeInlayFunctionLikeReturnTypeHints = true,
+          includeInlayEnumMemberValueHints = true,
+        },
+      },
+    },
   },
 
   ty = {
